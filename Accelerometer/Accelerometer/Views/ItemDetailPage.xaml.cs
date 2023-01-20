7502f1edbe30a6402da624a13dd8fe1c6d37ffdc
bgreer5050 <bgreer5050@gmail.com>
2023-01-19 20:18:31 -0500

    Added project files

---
+++ Accelerometer/Accelerometer/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Accelerometer.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Accelerometer.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Accelerometer/Accelerometer/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Accelerometer.Models;
+using Accelerometer.ViewModels;
+using Accelerometer.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Accelerometer.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
