c54521cc477c3ca7659c64d64029bf9ea455ea98
bgreer5050 <bgreer5050@gmail.com>
2023-01-20 19:11:32 -0500

    Added code to limit UI change if the Accelerometer difference is less tha .007.

--- Accelerometer/Accelerometer/Views/AccelerometerPage.xaml.cs
+++ Accelerometer/Accelerometer/Views/AccelerometerPage.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using Plugin.SimpleAudioPlayer;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
@@ -16,24 +18,52 @@ namespace Accelerometer.Views
     {
         private ISimpleAudioPlayer _simpleAudioPlayer;
         SensorSpeed speed = SensorSpeed.UI;
+        bool IsBusy = false;
+
+        float xDiff = 0;
+        float zDiff = 0;
+        float yDiff = 0;
+
         public AccelerometerPage()
         {
             InitializeComponent();
             _simpleAudioPlayer = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
             Stream beepStream = GetType().Assembly.GetManifestResourceStream("Accelerometer.beep.mp3");
+
+            //TODO Fix the beepStream null issue.
             bool isSuccess = _simpleAudioPlayer.Load(beepStream);
             Xamarin.Essentials.Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
             Xamarin.Essentials.Accelerometer.Start(speed);
-
-
         }
 
         private void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
         {
-            Device.BeginInvokeOnMainThread(() => {
-                var data = e.Reading;
-                ReadingsText.Text = $"Reading: X: {data.Acceleration.X}, Y: {data.Acceleration.Y}, Z: {data.Acceleration.Z}";
-            });
+
+            var data = e.Reading;
+
+            if ((xDiff - data.Acceleration.X) > .007 || (zDiff - data.Acceleration.Z) > .007 || (yDiff - data.Acceleration.Y) > .007)
+            {
+
+                if (!IsBusy)
+                {
+                    IsBusy = true;
+                    Device.BeginInvokeOnMainThread(() =>
+                {
+                    {
+                        ReadingsText.Text = $"Reading: X: {data.Acceleration.X}, Y: {data.Acceleration.Y}, Z: {data.Acceleration.Z}";
+                    }
+                    //System.Threading.Thread.Sleep(2000);
+                    IsBusy = false;
+                });
+                    xDiff = data.Acceleration.X;
+                    yDiff = data.Acceleration.Y;
+                    zDiff = data.Acceleration.Z;
+                }
+                else
+                {
+                    Debug.WriteLine("BUSY");
+                }
+            }
         }
 
         private void MonitorSwitch_Toggled(object sender, ToggledEventArgs e)
